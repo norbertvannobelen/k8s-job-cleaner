541a7a40f69c0474eb6102b156ddb5f4091cac99
Norbert van Nobelen <nvnobelen@Norberts-MacBook-Pro.local>
2020-01-26 16:55:20 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# k8s-job-cleaner
+# job-cleaner
+
+Removes completed jobs to try to prevent hitting the 10k job limit in GKE.
+
+By supplying a namespace (launch the app into a namespace, or replace the namespace variable), specific namespaces can be managed.
+
+## Process
+
+### Jobs
+
+The process inspects all the jobs on completion, and removes all the jobs which are not running. Reason of failure (for now) is ignored, however failed executions are logged.
+This process will also delete it's own old jobs, so observability is low. The use of a log aggregator might be required to keep insight in job failures.
+
+The process itself is a kubernetes cronjob, which launches a job to execute its tasks. The inherent problem which this has, is that if the number of ran jobs starts to near 10k/day, is that this process itself will also fail. A higher run frequency (aka multiple times a day), could stretch this process to multitudes of the 10k/day limit.
+
+### Pods
+
+Pods on most k8s systems can run in the 100k+ numbers, however all stopped pods take up disk space, and can clog the overview of pods. The job-cleaner for this reason also cleans pods which have completed.
+
+## Build & run
+
+There is a sample kubernetes cronjob yaml file in the buildAndRun directory. No dockerfile has been provided and no docker image is available for security reasons.
+
+The cronjob file provided has been tested with the application. A memory setting of 300MB seemed sufficient not to hit OOM.
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	batchV1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var (
+	clientset *kubernetes.Clientset
+)
+
+func init() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatalf("ERROR: init(): Could not get kube config in cluster. Error:" + err.Error())
+	}
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("ERROR: init(): Could not connect to kube cluster with config. Error:" + err.Error())
+	}
+}
+
+func main() {
+	cleanJobs(os.Getenv("NAMESPACE"))
+	time.Sleep(time.Second * 10)
+	cleanPods(os.Getenv("NAMESPACE"))
+	time.Sleep(time.Minute)
+}
+
+// cleanJobs - Filters for successful completions
+// Trigger brute force filter for failures, with risk of out of memory. By removing completed jobs first, this risk is reduced
+// Risk of out of memory is also managed by the whole reason of this program: Hitting a 10k job limit
+func cleanJobs(namespace string) {
+	removeJobs(namespace, "status.successful==1")
+	removeJobs(namespace, "")
+}
+
+func removeJobs(namespace string, filter string) {
+	list, err := clientset.BatchV1().Jobs(namespace).List(metav1.ListOptions{FieldSelector: filter})
+	if err != nil {
+		log.Fatalf("ERROR: cleanJobs(): Can not list jobs for namespace %s. Error %v", namespace, err)
+	}
+	// Clean up the jobs
+	for _, x := range list.Items {
+		// inspect job status: No pods should be active (status.active=0), job should be complete or failed
+		if x.Status.Active == 0 {
+			for _, s := range x.Status.Conditions {
+				if s.Type == batchV1.JobComplete || s.Type == batchV1.JobFailed {
+					log.Printf("INFO: cleanJobs(): Deleting job %s with status %s", x.Name, s.Type)
+					if err := clientset.BatchV1().Jobs(namespace).Delete(x.Name, &metav1.DeleteOptions{}); err != nil {
+						log.Printf("INFO: cleanJobs(): failed to delete job %s. Error %v", x.Name, err)
+					}
+				}
+			}
+		}
+	}
+}
+
+// cleanPods - Wraps removePods to add filters reducing memory use and passing filtering load to kubernetes master
+func cleanPods(namespace string) {
+	removePods(namespace, "status.phase=Succeeded")
+	removePods(namespace, "status.phase=Failed")
+}
+
+// removePods - Removes pods from kubernetes cluster according to provided filter
+func removePods(namespace string, filter string) {
+	list, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{FieldSelector: filter})
+	if err != nil {
+		log.Fatalf("ERROR: cleanPods(): Can not list pods for namespace %s. Error %v", namespace, err)
+	}
+	// Clean up the pods
+	for _, x := range list.Items {
+		// inspect pod status:
+		log.Printf("INFO: cleanPods(): pod %s with status %s removed", x.Name, x.Status.Phase)
+		if err := clientset.CoreV1().Pods(namespace).Delete(x.Name, &metav1.DeleteOptions{}); err != nil {
+			log.Printf("INFO: cleanPods(): failed to delete job %s. Error %v", x.Name, err)
+		}
+	}
+}
